f3e6a3a4e597c8aa973e1c3cb30018497432758b
LitWebloc <webloc.com@yandex.by>
2018-08-23 13:01:18 +0300

    HomePage

---
+++ js/slider.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+  $('.slider-run').slick({
+  slidesToShow: 9,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 1670,
+      settings: {
+        slidesToShow: 7,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 1320,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 1120,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 970,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 820,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+});
